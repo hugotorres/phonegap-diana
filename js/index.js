0ef6a88a529cdf97bdce1576f4a42d8f89ec224c
hugotorres <hugotorreslinares@gmail.com>
2015-08-24 14:07:47 -0500

    con google auth para pruebas

--- js/index.js
+++ js/index.js
@@ -16,6 +16,13 @@
  * specific language governing permissions and limitations
  * under the License.
  */
+ var googleapi = {
+    authorize: function(options) {
+        var deferred = $.Deferred();
+        deferred.reject({ error: 'Not Implemented' });
+        return deferred.promise();
+    }
+};
 var app = {
     SOME_CONSTANTS : false,  // some constant
 
@@ -42,7 +49,84 @@ var app = {
     onDeviceReady: function() {
         console.log("device ready, start making you custom calls!");
 
-        // Start adding your code here....
+			 var $loginButton = $('#login a');
+    var $loginStatus = $('#login p');
+
+    $loginButton.on('click', function() {
+        googleapi.authorize({
+            client_id: '446897412892-q1srpupi16rpunmrb947gb6q1er7ftfe.apps.googleusercontent.com',
+            client_secret: 'iBYFAy4t1lGz7iNDDCZFmKMo',
+            redirect_uri: 'http://127.0.0.1',
+            scope: 'https://www.googleapis.com/auth/analytics.readonly'
+        }).done(function(data) {
+            $loginStatus.html('Access Token: ' + data.access_token);
+        }).fail(function(data) {
+            $loginStatus.html(data.error);
+        });
+    });
 
     }
 };
+
+
+
+
+var googleapi = {
+    authorize: function(options) {
+        var deferred = $.Deferred();
+
+        //Build the OAuth consent page URL
+        var authUrl = 'https://accounts.google.com/o/oauth2/auth?' + $.param({
+            client_id: options.client_id,
+            redirect_uri: options.redirect_uri,
+            response_type: 'code',
+            scope: options.scope
+        });
+
+        //Open the OAuth consent page in the InAppBrowser
+        var authWindow = window.open(authUrl, '_blank', 'location=no,toolbar=no');
+
+        //The recommendation is to use the redirect_uri "urn:ietf:wg:oauth:2.0:oob"
+        //which sets the authorization code in the browser's title. However, we can't
+        //access the title of the InAppBrowser.
+        //
+        //Instead, we pass a bogus redirect_uri of "http://localhost", which means the
+        //authorization code will get set in the url. We can access the url in the
+        //loadstart and loadstop events. So if we bind the loadstart event, we can
+        //find the authorization code and close the InAppBrowser after the user
+        //has granted us access to their data.
+        $(authWindow).on('loadstart', function(e) {
+            var url = e.originalEvent.url;
+            var code = /\?code=(.+)$/.exec(url);
+            var error = /\?error=(.+)$/.exec(url);
+
+            if (code || error) {
+                //Always close the browser when match is found
+                authWindow.close();
+            }
+
+            if (code) {
+                //Exchange the authorization code for an access token
+                $.post('https://accounts.google.com/o/oauth2/token', {
+                    code: code[1],
+                    client_id: options.client_id,
+                    client_secret: options.client_secret,
+                    redirect_uri: options.redirect_uri,
+                    grant_type: 'authorization_code'
+                }).done(function(data) {
+                    deferred.resolve(data);
+                }).fail(function(response) {
+                    deferred.reject(response.responseJSON);
+                });
+            } else if (error) {
+                //The user denied access to the app
+                deferred.reject({
+                    error: error[1]
+                });
+            }
+        });
+
+        return deferred.promise();
+    }
+};
+
